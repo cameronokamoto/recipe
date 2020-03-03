8b07c9c55f06be66c0aae98b1364bd8374d1ebcd
Lance Haderlie <lance.haderlie@gmail.com>
2020-03-03 16:14:08 -0700

    Error checking

--- index.js
+++ index.js
@@ -23,22 +23,40 @@ function mainCtrl ($scope) {
   };
 }
 
-var app = angular.module("myIngredientsList", []); 
+var app = angular.module("AddRecipe", []); 
 app.controller("myCtrl", function($scope, $window) 
 {
     $scope.ingredients = [];
     $scope.recipe = new recipe;
+    // Called when the "Add Recipe" button is called, gets what's currently in 
+    // the instructions text box (Will ask if its going to change what was already input)
+    // and then goes to success.html
     $scope.addToRecipes = function()
     {
         if($scope.ingredients.length == 0)
         {
             $scope.addIngredient();
         }
-        console.log("In addToRecipes");
         $scope.addInstruction();
-        console.log($scope.recipe);
-        //$window.location.href = "success.html";
+        if($scope.recipe.instructions == "")
+        {
+            if(confirm("Your recipe instructions are empty, is that OK?")){}
+            else{return;}
+        }
+        if($scope.recipe.ingredients.length == 0)
+        {
+            if(confirm("Your recipe ingredients are empty, are you sure that's right?")){}
+            else{return;}
+        }
+        if($scope.recipe.imageUrl == "")
+        {
+            if(confirm("You don't have an image for your recipe, is that OK?")){}
+            else{return;}
+        }
+        $window.location.href = "success.html";
     }
+    // Add an ingredient to the list of ingredients, OR process a multi-line list of ingredients
+    // into an array of ingredients, then assign it to the recipe we are holding in the scope
     $scope.addIngredient = function () 
     {
         $scope.errortext = "";
@@ -56,6 +74,9 @@ app.controller("myCtrl", function($scope, $window)
         document.getElementById("ingInput").value = "";
         $scope.recipe.ingredients = $scope.ingredients;
     }
+    // Grab the image URL from the input field and assign it properly. 
+    // If its empty don't change things, OR if the current string is a 
+    // substring of the current url (they are backspacing) don't change things
     $scope.addURL = function()
     {
       if($scope.imageURL == "" || $scope.recipe.imageUrl.includes($scope.imageURL)){}
@@ -64,6 +85,8 @@ app.controller("myCtrl", function($scope, $window)
         $scope.recipe.imageUrl = $scope.imageURL;
       }
     }
+    // The enter key should trigger the addIngredients function while in the 
+    // ingredients text box
     $scope.keyPress = function($event)
     {
       var key = $event.keyCode;
@@ -72,11 +95,15 @@ app.controller("myCtrl", function($scope, $window)
         $scope.addIngredient();
       }
     }
+    // Remove the ingredient chosen
     $scope.removeIngredient = function (x) 
     {
         $scope.errortext = "";    
         $scope.ingredients.splice(x, 1);
     }
+    // Assign the text in the instructions box to the recipe's instructions
+    // Check if they are different and ask for confirmation before changing them.
+    // If its empty don't do anything
     $scope.addInstruction = function () 
     {
       if($scope.instructionsText == "") {}
